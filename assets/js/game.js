353078381bb4222ffbce114405fd0efd760daea8
filleben <filleryben@gmail.com>
2020-05-29 02:08:35 +0100

    added functions to view scores and instructions

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
     $('#start').click(function () {
         $(this).parent().removeClass('visible');
     });
+    $("#your-scores").click(function(){
+        $("#saved-scores").slideToggle("slow");
+    });
+    $("#how-to-play").click(function(){
+        $("#instructions").slideToggle("slow");
+    });
 });
